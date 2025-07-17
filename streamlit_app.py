c92e244a73e53fcfba5da31a47e3664d891191cf
chyi <yi-chung.chen@empa.ch>
2025-07-17 15:38:24 +0200

    testing graphviz

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,14 @@ if name:
     
 number = st.slider("Pick a number", 0, 100)
 st.write(f"You selected: {number}")
+
+
+import streamlit as st
+from graphviz import Digraph
+
+dot = Digraph()
+dot.node('A', 'Start')
+dot.node('B', 'End')
+dot.edge('A', 'B')
+
+st.graphviz_chart(dot.source)
